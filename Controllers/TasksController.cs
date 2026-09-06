af9ae0ac022c500571d01cd05b5341d9fc488438
Pratham Karki <you@example.com>
2026-09-06 19:41:53 +0545

    Completion of controller scaffolding

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,149 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TraineeMVC.Models;
+
+public class TasksController : Controller
+{
+    private readonly TraineeDbContext _context;
+
+    public TasksController(TraineeDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: TASKS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.Employees.ToListAsync());
+    }
+
+    // GET: TASKS/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var task = await _context.Employees
+            .FirstOrDefaultAsync(m => m.id == id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        return View(task);
+    }
+
+    // GET: TASKS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: TASKS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("id,Title,Description,IsCompleted")] TraineeMVC.Models.Task task)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(task);
+    }
+
+    // GET: TASKS/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var task = await _context.Employees.FindAsync(id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+        return View(task);
+    }
+
+    // POST: TASKS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, [Bind("id,Title,Description,IsCompleted")] TraineeMVC.Models.Task task)
+    {
+        if (id != task.id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(task);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(task.id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(task);
+    }
+
+    // GET: TASKS/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var task = await _context.Employees
+            .FirstOrDefaultAsync(m => m.id == id);
+        if (task == null)
+        {
+            return NotFound();
+        }
+
+        return View(task);
+    }
+
+    // POST: TASKS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var task = await _context.Employees.FindAsync(id);
+        if (task != null)
+        {
+            _context.Employees.Remove(task);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool TaskExists(int? id)
+    {
+        return _context.Employees.Any(e => e.id == id);
+    }
+}
